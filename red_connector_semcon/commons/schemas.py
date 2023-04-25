d6d1a67562b88ede8a220a060b03e94ab45b5f58
Dominik Probst <Dominik.Probst@htw-berlin.de>
2023-04-25 11:16:21 +0200

    specify schema for receive-file and send-file

--- red_connector_semcon/commons/schemas.py
+++ red_connector_semcon/commons/schemas.py
@@ -4,9 +4,10 @@ from copy import deepcopy
 _HTTP_METHODS = ['Get', 'Put', 'Post']
 _HTTP_METHODS_ENUMS = deepcopy(_HTTP_METHODS) + [m.lower() for m in _HTTP_METHODS] + [m.upper() for m in _HTTP_METHODS]
 
+_RESOURCE_TYPE_ENUMS = ['id', 'dri', 'schema_dri', 'table']
 _OUTPUT_FORMAT_ENUMS = ['plain', 'full', 'meta', 'validation']
 
-SCHEMA = {
+_BASE_SCHEMA = {
     'type': 'object',
     'properties': {
         'url': {'type': 'string'},
@@ -21,20 +22,33 @@ SCHEMA = {
             'additionalProperties': False,
             'required': ['username', 'password']
         },
-        'resource': {
-            'type': 'object',
-            'properties': {
-                'id': {'type': 'string'},
-                'dri': {'type': 'string'},
-                'schemaDri': {'type': 'string'},
-                'tableName': {'type': 'string'},
-                'key': {'type': 'string'},
-                'format': {'enum': _OUTPUT_FORMAT_ENUMS}
-            },
-            'additionalProperties': False
-        },
         'disableSSLVerification': {'type': 'boolean'}
     },
     'additionalProperties': False,
     'required': ['url']
+}
+
+RECEIVE_FILE_SCHEMA = deepcopy(_BASE_SCHEMA)
+RECEIVE_FILE_SCHEMA['resource'] = {
+    'type': 'object',
+    'properties': {
+        'resource_type': {'enum': _RESOURCE_TYPE_ENUMS},
+        'resource_value': {'type': 'string'},
+        'key': {'type': 'string'},
+        'format': {'enum': _OUTPUT_FORMAT_ENUMS}
+    },
+    'additionalProperties': False,
+    'required': ['resource_type', 'resource_value']
+}
+
+SEND_FILE_SCHEMA = deepcopy(_BASE_SCHEMA)
+RECEIVE_FILE_SCHEMA['resource'] = {
+    'type': 'object',
+    'properties': {
+        'dri': {'type': 'string'},
+        'schema_dri': {'type': 'string'},
+        'table': {'type': 'string'},
+        'key': {'type': 'string'}
+    },
+    'additionalProperties': False
 }
